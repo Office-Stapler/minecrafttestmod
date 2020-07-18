7ece138e60a95866c2f46bf12ace4963175761b0
Office-Stapler <raymond.lam@outlook.com.au>
2020-07-18 15:50:46 +1000

    Update

--- src/main/java/com/pandafear/testmod/testmod.java
+++ src/main/java/com/pandafear/testmod/testmod.java
@@ -1,15 +1,23 @@
 package com.pandafear.testmod;
 
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.pandafear.testmod.world.gen.RubyOreGen;
 
 @Mod(testmod.MODID)
 public class testmod {
 	public final static String MODID = "testmod";
-	private static final Logger LOGGER = LogManager.getLogger();
+	public static final Logger LOGGER = LogManager.getLogger();
 	public testmod() {
 		LOGGER.debug("Hello");
 	}
-	
+	@SubscribeEvent
+	public static void loadCompleteEvent(FMLLoadCompleteEvent event) {
+		RubyOreGen.generateOre();
+	}
 }
--- src/main/java/com/pandafear/testmod/world/gen/RubyOreGen.java
+++ src/main/java/com/pandafear/testmod/world/gen/RubyOreGen.java
@@ -1,7 +1,31 @@
 package com.pandafear.testmod.world.gen;
 
+import com.pandafear.testmod.testmod;
+// import com.pandafear.testmod.blocks.Ruby_Ore;
+
+import init.ModBlocks;
+import net.minecraft.world.biome.Biome;
+// import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.placement.ConfiguredPlacement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class RubyOreGen {
 	public static void generateOre() {
-		 
+		 for (Biome biome : ForgeRegistries.BIOMES) {
+			ConfiguredPlacement<?> customConfig = Placement.COUNT_RANGE.configure(
+			new CountRangeConfig(20, 5, 5, 70));
+			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
+				new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE,
+				ModBlocks.RUBY_ORE.getDefaultState(),
+					10))
+					.withPlacement(customConfig));
+		 }
+		 testmod.LOGGER.debug("DEBUGGING....");
+		 testmod.LOGGER.debug(ModBlocks.RUBY_ORE);
 	}
 }
